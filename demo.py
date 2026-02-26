a2c472215c3acd6eff8108c94ef2d8d5cdbc54d6
Chen-An Wang <willwang22@gmail.com>
2026-02-26 15:17:41 -0800

    Fix: Local audio files path resolution in Gradio 4

--- demo.py
+++ demo.py
@@ -285,7 +285,9 @@ def recommend_for_client(mood, lang):
         local_path = get_local_audio_path(filename)
         
         if local_path:
-            player = f'<div style="margin: 8px 0;"><audio controls src="/file={local_path}" style="width:100%"></audio></div>'
+            abs_path = os.path.abspath(local_path)
+            safe_path = urllib.parse.quote(abs_path)
+            player = f'<div style="margin: 8px 0;"><audio controls src="/file={safe_path}" style="width:100%"></audio></div>'
         else:
             video_id = get_youtube_video_id(title)
             player = build_player_html(title, video_id, lang)
@@ -403,7 +405,9 @@ def recommend_for_musician(mood, lang):
         
         local_path = get_local_audio_path(filename)
         if local_path:
-            player = f'<div style="margin: 8px 0;"><audio controls src="/file={local_path}" style="width:100%"></audio></div>'
+            abs_path = os.path.abspath(local_path)
+            safe_path = urllib.parse.quote(abs_path)
+            player = f'<div style="margin: 8px 0;"><audio controls src="/file={safe_path}" style="width:100%"></audio></div>'
         else:
             video_id = get_youtube_video_id(title)
             player = build_player_html(title, video_id, lang)
@@ -508,4 +512,4 @@ with gr.Blocks(title="Timbre Audio-to-Brief Engine") as demo:
     client_btn.click(fn=recommend_for_client, inputs=[mood_input, lang_state], outputs=outputs)
     musician_btn.click(fn=recommend_for_musician, inputs=[mood_input, lang_state], outputs=outputs)
 
-demo.launch(allowed_paths=["songs", "../songs"])
+demo.launch(allowed_paths=[os.path.abspath("songs"), os.path.abspath("../songs")])
